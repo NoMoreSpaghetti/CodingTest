580f496d5df36e5ddd1d0d00abdd8be2ed7b655e
NoMoreSpaghetti <h1103095@gmail.com>
2022-03-28 21:36:53 +0900

    Create 1292.cpp

---
+++ baekjoon/1292.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    int sum = 0;
+    int num = 0;
+    int i = 1;
+
+    while(true) {
+        num += i;
+        if(A <= num) {
+            break;
+        }
+        i++;
+    }
+    
+    if(num >= B) {
+        sum += i * (B - A + 1);
+    } else {
+        sum += i * (num - A + 1);
+        i++;
+        while(true) {
+            num += i;
+            if(num >= B) {
+                sum += i * (B - (num - i));
+                break;
+            } else {
+                sum += i * i;
+            }
+            i++;
+        }
+    }
+    
+
+    
+
+    cout << sum << '\n';
+}
